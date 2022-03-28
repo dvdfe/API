1867905d9f957e2994700ae91b99a9b14b994e61
DVDFE <davidfe.pro@gmail.com>
2022-03-28 13:15:26 +0200

    maj

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const quote = document.getElementById("quote")
 fetch("https://api.quotable.io/random")
 .then(res => res.json())
 .then(data => {
+    
     quote.innerHTML = data.content
 })
-
-
-
-
-
-
-
-
-
