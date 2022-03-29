122ab7a51613eb53b15ea2ec6ba24ce2a20fa8c3
DVDFE <davidfe.pro@gmail.com>
2022-03-29 13:15:13 +0200

    commit

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const quote = document.getElementById("quote")
 
 fetch("https://api.quotable.io/random")
 .then(res => res.json())
-.then(data => {
-    
-    quote.innerHTML = data.content
+.then(data => { addQuote(data)
 })
+
+function addQuote(quote){
+    const quoteTag = document.getElementById("quote")
+    const h2 = makeH2(quote.content)
+    const p = makeP(quote.author)
+    quoteTag.appendChild(h2)
+    quoteTag.appendChild(p)
+}
+
+function makeH2(content){
+    const h2 = document.createElement('h2');
+    h2.innerHTML = content
+    return h2
+}
+
+function makeP(author){
+    const p = document.createElement('p')
+    p.innerHTML = author
+    return p
+}
